31a2832c680ce78044ac83570d2d028087ee4107
Lucashlfr <lucas.helfer@gmx.net>
2025-01-11 03:03:30 +0100

    add Jenkinsfile

--- src/main/java/com/messdiener/cms/v3/CMSXApplication.java
+++ src/main/java/com/messdiener/cms/v3/CMSXApplication.java
@@ -14,5 +14,6 @@ public class CMSXApplication {
 
         Cache.getPersonService().updateUsers();
         Cache.getUserService().createUserInSecurity();
+        System.out.println("Test");
     }
 }
